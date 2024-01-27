f7ca095c55f001b3a986914a17773069d75e3190
Andrea Bertanzon <andrea.bertanzon@gmail.com>
2024-01-27 11:17:06 +0000

    tailwind css setup

--- main.go
+++ main.go
@@ -11,10 +11,11 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		helloComponent := templates.Hello("Andrea")
+		helloComponent := templates.Index()
 		helloComponent.Render(context.Background(), c.Response().Writer)
 		return nil
 	})
+	e.Static("/css", "css")
 	e.Static("/static", "static")
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":8081"))
 }
